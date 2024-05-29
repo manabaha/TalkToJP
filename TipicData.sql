3a61f2116bd4541d63f40541463005c9f3367752
manabaha <165973071+manabaha@users.noreply.github.com>
2024-05-29 16:02:51 +0900

    Add files via upload

---
+++ TipicData.sql
@@ -0,0 +1,69 @@
+INSERT INTO ttjp_topic (to_idx, to_name, to_content, picture1)
+VALUES (1, '즐거운 일본어 학습', '재미있고 효과적인 일본어 학습 방법을 소개합니다!', 't1.png');
+
+INSERT INTO ttjp_topic (to_idx, to_name, to_content, picture1)
+VALUES (2, '일본어 기초 문법', '일본어의 기본 문법을 쉽게 이해하는 방법을 배워봅시다!', 't2.png');
+
+INSERT INTO ttjp_topic (to_idx, to_name, to_content, picture1)
+VALUES (3, '한국인을 위한 일본어 발음', '일본어를 잘 발음하는 법을 익혀봅시다!', 't3.png');
+
+INSERT INTO ttjp_topic (to_idx, to_name, to_content, picture1)
+VALUES (4, '일본어로 일상 생활 표현', '일본어로 일상 생활에서 자주 사용되는 표현을 배워봅시다!', 't4.png');
+
+INSERT INTO ttjp_topic (to_idx, to_name, to_content, picture1)
+VALUES (5, '일본어로 여행하기', '여행 중에 유용한 일본어 표현과 문장을 배워봅시다!', 't5.png');
+
+INSERT INTO ttjp_topic (to_idx, to_name, to_content, picture1)
+VALUES (6, '일본어로 읽는 소설 추천', '일본어 실력 향상을 위해 추천하는 소설들을 소개합니다!', 't6.png');
+
+INSERT INTO ttjp_topic (to_idx, to_name, to_content, picture1)
+VALUES (7, '일본어 독해 연습', '다양한 주제의 일본어 독해 연습을 해봅시다!', 't7.png');
+
+INSERT INTO ttjp_topic (to_idx, to_name, to_content, picture1)
+VALUES (8, '일본어 쓰기 연습', '다양한 주제로 일본어로 글을 써보며 실력을 향상해봅시다!', 't8.png');
+
+INSERT INTO ttjp_topic (to_idx, to_name, to_content, picture1)
+VALUES (9, '일본어 회화 연습', '다양한 상황에서의 일본어 회화 연습을 해봅시다!', 't9.png');
+
+INSERT INTO ttjp_topic (to_idx, to_name, to_content, picture1)
+VALUES (10, '일본어 단어 암기', '일본어 단어 암기를 위한 다양한 방법을 알아봅시다!', 't10.png');
+
+INSERT INTO ttjp_topic (to_idx, to_name, to_content, picture1)
+VALUES (11, '일본어로 노래 부르기', '일본어로 노래를 부르며 발음과 억양을 연습해봅시다!', 't11.png');
+
+INSERT INTO ttjp_topic (to_idx, to_name, to_content, picture1)
+VALUES (12, '일본어 문장 만들기 연습', '다양한 주제로 일본어 문장을 만들어봅시다!', 't12.png');
+
+INSERT INTO ttjp_topic (to_idx, to_name, to_content, picture1)
+VALUES (13, '일본어 자동사와 타동사', '일본어의 자동사와 타동사에 대해 배워봅시다!', 't13.png');
+
+INSERT INTO ttjp_topic (to_idx, to_name, to_content, picture1)
+VALUES (14, '일본어 숫자와 계산', '일본어로 숫자를 읽고 계산하는 방법을 배워봅시다!', 't14.png');
+
+INSERT INTO ttjp_topic (to_idx, to_name, to_content, picture1)
+VALUES (15, '일본어 문화와 전통', '다양한 일본의 문화와 전통에 대해 알아봅시다!', 't15.png');
+
+INSERT INTO ttjp_topic (to_idx, to_name, to_content, picture1)
+VALUES (16, '일본어로 사고하기', '일본어로 생각하는 방식을 익혀봅시다!', 't16.png');
+
+INSERT INTO ttjp_topic (to_idx, to_name, to_content, picture1)
+VALUES (17, '한자 학습하기', '일본어에서 사용되는 한자를 학습해봅시다!', 't17.png');
+
+INSERT INTO ttjp_topic (to_idx, to_name, to_content, picture1)
+VALUES (18, '일본어 속담과 관용구', '유용한 일본어 속담과 관용구를 배워봅시다!', 't18.png');
+
+INSERT INTO ttjp_topic (to_idx, to_name, to_content, picture1)
+VALUES (19, '일본어로 일기 쓰기', '일본어로 감정과 생각을 표현하는 법을 배워봅시다!', 't19.png');
+
+INSERT INTO ttjp_topic (to_idx, to_name, to_content, picture1)
+VALUES (20, '일본어로 취미 생활하기', '다양한 취미 활동을 일본어로 즐겨봅시다!', 't20.png');
+
+
+UPDATE ttjp_topic
+SET picture2 = 'p2.png',
+    picture3 = 'p3.png',
+    picture4 = 'p4.png',
+    picture5 = 'p5.png',
+    picture6 = 'p6.png';
+
+commit;
